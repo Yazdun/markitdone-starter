694158f4f199ed7d7f1432cf4acf2d8ea093820f
Yazdun <yazduntube@gmail.com>
2023-05-20 18:52:10 +0430

    feature: complete carousel section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 (function () {
-  "use strict";
+  ("use strict");
   document.addEventListener("DOMContentLoaded", function () {
     // ::: ADDING COLOR TO NAVBAR ON SCROLL :::
     window.addEventListener("scroll", function () {
@@ -29,5 +29,72 @@
   closeBtn.addEventListener("click", function () {
     mobileMenuOverlay.classList.remove("mobile-menu-overlay--open");
     mobileMenu.classList.remove("mobile-menu--open");
+  });
+
+  // ::: CAROUSEL :::
+  const carouselItems = document.querySelectorAll(".c-carousel-item");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+  let currentIndex = 0;
+  let isAnimating = false;
+
+  // Show the initial item
+  carouselItems[currentIndex].classList.add("active");
+
+  // Function to show the next item
+  function showNextItem() {
+    if (isAnimating) return;
+    isAnimating = true;
+    carouselItems[currentIndex].classList.remove("active");
+    carouselItems[currentIndex].classList.add("prev");
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    carouselItems[currentIndex].classList.add("next");
+    setTimeout(() => {
+      carouselItems[currentIndex].classList.add("active");
+      carouselItems[currentIndex].classList.remove("next");
+      carouselItems[currentIndex].addEventListener(
+        "transitionend",
+        () => {
+          carouselItems[currentIndex].classList.remove("prev");
+          isAnimating = false;
+        },
+        { once: true }
+      );
+    }, 10);
+  }
+
+  // Function to show the previous item
+  function showPrevItem() {
+    if (isAnimating) return;
+    isAnimating = true;
+    carouselItems[currentIndex].classList.remove("active");
+    carouselItems[currentIndex].classList.add("next");
+    currentIndex =
+      (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    carouselItems[currentIndex].classList.add("prev");
+    setTimeout(() => {
+      carouselItems[currentIndex].classList.add("active");
+      carouselItems[currentIndex].classList.remove("prev");
+      carouselItems[currentIndex].addEventListener(
+        "transitionend",
+        () => {
+          carouselItems[currentIndex].classList.remove("next");
+          isAnimating = false;
+        },
+        { once: true }
+      );
+    }, 10);
+  }
+
+  // Event listener for next button
+  nextBtn.addEventListener("click", function () {
+    showNextItem();
+    resetAutoShowTimer();
+  });
+
+  // Event listener for previous button
+  prevBtn.addEventListener("click", function () {
+    showPrevItem();
+    resetAutoShowTimer();
   });
 })();
