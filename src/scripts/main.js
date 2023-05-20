d2b8abe919252cf15bc1c5bf9bea6d1e437643e8
Yazdun <yazduntube@gmail.com>
2023-05-20 15:20:29 +0430

    feat: create navigation bar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,33 @@
 /* eslint-env browser */
-(function() {
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function() {
-    // Your custom JavaScript goes here
+(function () {
+  "use strict";
+  document.addEventListener("DOMContentLoaded", function () {
+    // ::: ADDING COLOR TO NAVBAR ON SCROLL :::
+    window.addEventListener("scroll", function () {
+      let navbar = document.getElementById("header");
+      let scrollPosition = window.scrollY;
+
+      if (scrollPosition > 100) {
+        navbar.classList.add("header__scrolled");
+      } else {
+        navbar.classList.remove("header__scrolled");
+      }
+    });
+  });
+
+  // ::: MOBILE MENU :::
+  let hamburgerBtn = document.getElementById("burger");
+  let mobileMenuOverlay = document.querySelector(".mobile-menu-overlay");
+  let closeBtn = document.querySelector(".mobile-menu-overlay__close-btn");
+  let mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerBtn.addEventListener("click", function () {
+    mobileMenuOverlay.classList.add("mobile-menu-overlay--open");
+    mobileMenu.classList.add("mobile-menu--open");
+  });
+
+  closeBtn.addEventListener("click", function () {
+    mobileMenuOverlay.classList.remove("mobile-menu-overlay--open");
+    mobileMenu.classList.remove("mobile-menu--open");
   });
 })();
